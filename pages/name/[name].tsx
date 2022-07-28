e036d6fb70642444d696d7a83d3ae3806e044c3b
Brawer Nuñez <n.eedar@hotmail.com>
2022-07-28 15:29:23 -0500

    add ISG & ISR

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -120,7 +120,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
     paths: data.results.map(({name}) => ({
       params: { name }
     })),
-    fallback: false
+    fallback: 'blocking'
   }
 }
 
@@ -128,11 +128,24 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 
   const { name } = params as { name: string }
 
-  return {
-    props: {
-      pokemon: await getPokemonInfo( name )
+  const pokemon = await getPokemonInfo( name )
+
+  if ( !pokemon ) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
     }
   }
+
+  return {
+    props: {
+      pokemon
+    },
+    revalidate: 86400
+  }
+
 }
 
 export default PokemonByNamePage
